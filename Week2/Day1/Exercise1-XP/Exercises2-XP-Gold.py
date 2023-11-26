7740da9d4cc48cfbee716af808a013e9470b6d27
Sveta Livshits <nekto.nap@gmail.com>
2023-11-26 16:08:48 +0200

    Exercises XP Gold done

--- Week2/Day1/Exercise1-XP/Exercises2-XP-Gold.py
+++ Week2/Day1/Exercise1-XP/Exercises2-XP-Gold.py
@@ -1,1 +1,15 @@
+# Exercise 1
 print ("Hello world\n"*4+"I love python\n"*4)
+
+# Exercise 2
+month = input ("What number of month now?")
+if int(month)>=3 and int(month)<=5:
+    print("Spring")
+elif int(month)>=6 and int(month)<=8:
+    print("Summer")
+elif int(month)>=9 and int(month)<=11:
+    print("Autumn")   
+elif int(month)==1 or int(month)==2 or int(month)==12:
+    print("Winter")
+else:
+    print("Wrong number")
