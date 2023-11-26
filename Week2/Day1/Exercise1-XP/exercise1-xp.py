7a1e9009a5f561e3d0993c80cb5a6632e695f608
Sveta Livshits <nekto.nap@gmail.com>
2023-11-26 14:34:27 +0200

    week2 day1 exersise1-mandatory done

---
+++ Week2/Day1/Exercise1-XP/exercise1-xp.py
@@ -0,0 +1,53 @@
+# Exercise 1
+print ("Hello world\n"*4)
+
+
+# Exercise 2
+print (99**3*8)
+
+
+# Exercise 3
+#false
+# true
+#false
+#eror
+#false
+
+#Exercise 4
+computer_brand = "Asus"
+print (f"I have a {computer_brand} computer.")
+
+#Exercise 5
+name = "Sveta"
+age = "30"
+shoe_size = "42"
+info = f"Hi, my name is {name}, I'm just {age} year old, but I already have {shoe_size} shoe size"
+print (info)
+
+#Exercise 6
+a = 9
+b = 7
+if a>b:
+    print("Hello World")
+
+# Exercise 7
+number = input("Tell me a number:")
+if int(number)%2 == 0:
+    print ("even")
+else:
+    print ("odd")
+
+
+# Exercise 8
+yourName = input("Tell me your name: ")
+if yourName.lower() == "sveta":
+    print ("Wow, you are me")
+else:
+    print ("Wow, you are not me")
+
+# Exercise 9
+height = input ("Tell me your height: ")
+if int(height) > 145:
+    print("Ride!")
+else:
+    print("You need to grow some more")
