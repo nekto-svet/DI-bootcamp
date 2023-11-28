10eb996f6e3170b1e6b2d3b0704892b38710cd08
Sveta Livshits <nekto.nap@gmail.com>
2023-11-28 15:59:51 +0200

    week2 day3 daily challenge done

---
+++ Week2/Day3/Daily-Challenge-Dictionaries.py
@@ -0,0 +1,42 @@
+# Challenge 1
+user_word = input("Give me a word: ").lower()
+characters = {}
+for i, char in enumerate(user_word):
+    if char in characters:
+        characters[char] += [i]
+    else:
+        characters.update({char:[i]})
+
+print(characters)
+
+
+# Challenge 2
+import re
+
+items_purchase = {
+  "Apple": "$4",
+  "Honey": "$3",
+  "Fan": "$14",
+  "Bananas": "$4",
+  "Pan": "$100",
+  "Spoon": "$2"
+}
+
+wallet = "$100" 
+
+items_purchase_int = {}
+for item in items_purchase:
+   items_purchase_int[item] = int(re.sub("[$,]","",items_purchase[item]))
+ 
+wallet_int = int(wallet.replace("$",""))
+
+result = []
+
+for item in items_purchase_int:
+    if items_purchase_int[item]<wallet_int:
+        result.append(item)
+
+if result:
+    print(result)
+else:
+    print ("Nothing")
