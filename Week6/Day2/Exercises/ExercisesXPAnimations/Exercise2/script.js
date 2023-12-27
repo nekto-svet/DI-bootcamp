632242217abe90547fb2d5764c7e33fbe2e4407b
Sveta Livshits <nekto.nap@gmail.com>
2023-12-27 20:35:49 +0200

    Week2 Day2(Day1) ExerciseXP_Animation endless animalion added

--- Week6/Day2/Exercises/ExercisesXPAnimations/Exercise2/script.js
+++ Week6/Day2/Exercises/ExercisesXPAnimations/Exercise2/script.js
@@ -1,40 +1,89 @@
-// Copy the code above, to a structured HTML file.
-// In your Javascript file, use setInterval to move the <div id="animate"> to the right side of the <div id="container">, when the button is clicked on.
 
-const btn = document.querySelector('button');
+// In your Javascript file, use setInterval to move the <div id="animate"> to the right side of the <div id="container">, when the button is clicked on.
 
 function myMove(){
     const animate = document.getElementById('animate');
     let pos_left = 0;
     let pos_top = 0;
-    let pos_right = 350;
 
-    let id1 = setInterval(function(){
-        animate.style.left = pos_left+'px';
-        if (pos_left !== 350) {
-            animate.style.left = pos_left+'px';
-            pos_left++
-        } else {
-            clearInterval(id1);
-            let id3 = setInterval(function(){
-                if (pos_left == 350 && pos_right !== 0) {
-                    animate.style.left = pos_right+'px';
-                    pos_right--
-                } else {
-                    clearInterval(id3);
-                }
-            }, 1);
-        }
-    }, 1);
 
-    let id2 = setInterval(function(){
-        if (pos_top < 350) {
-            animate.style.top = pos_top+'px';
-            pos_top+=0.5
-        } else {
-            clearInterval(id2);
-        }
-    }, 1);
+
+    // let id1 = setInterval(function(){
+    //     animate.style.left = pos_left+'px';
+    //     if (pos_left !== 350) {
+    //         animate.style.left = pos_left+'px';
+    //         pos_left++
+    //     } else {
+    //         clearInterval(id1);
+    //         let id3 = setInterval(function(){
+    //             if (pos_left == 350 && pos_right !== 0) {
+    //                 animate.style.left = pos_right+'px';
+    //                 pos_right--
+    //             } else {
+    //                 clearInterval(id3);
+    //             }
+    //         }, 1);
+    //     }
+    // }, 1);
+
+
+    leftToRight()
+
+    function leftToRight(){
+        let id1 = setInterval(
+            function(){
+            if (pos_left < 350) {
+                animate.style.left = pos_left+'px';
+                pos_left+=0.6
+            } else {
+                clearInterval(id1);
+                rightToLeft()
+            }
+        }, 1);
+    }
+
+    function rightToLeft(){
+        let id2 = setInterval(
+            function(){
+            if (pos_left > 0) {
+                animate.style.left = pos_left+'px';
+                pos_left-=0.7
+            } else {
+                leftToRight()
+                clearInterval(id2);
+            }
+        }, 1);
+    }
+
+
+    topToBottom()
+
+    function topToBottom(){
+        let id3 = setInterval(
+            function(){
+            if (pos_top < 350) {
+                animate.style.top = pos_top+'px';
+                pos_top+=0.4
+            } else {
+                clearInterval(id3);
+                bottomToTop()
+            }
+        }, 1);
+    }
+
+    function bottomToTop(){
+        let id4 = setInterval(
+            function(){
+            if (pos_top > 0) {
+                animate.style.top = pos_top+'px';
+                pos_top-=0.3
+            } else {
+                topToBottom()
+                clearInterval(id4);
+            }
+        }, 1);
+    }
+    
 }
 
 
