8a18f1571a9ec59e2de10820ff6be7a4c520ac91
Sveta Livshits <nekto.nap@gmail.com>
2024-01-11 21:13:07 +0200

    Week8 Day2 ExerciseXP fixed

--- Week8/Day2/ExerciseXP/Exercise1_blog_api/server.js
+++ Week8/Day2/ExerciseXP/Exercise1_blog_api/server.js
@@ -2,16 +2,12 @@
 
 // Create a data array to simulate a database. Each item in the array should represent a blog post with properties like id, title, and content.
 
-// Implement the following routes using Express:
-// GET /posts: Return a list of all blog posts.
-// GET /posts/:id: Return a specific blog post based on its id.
-
-// POST /posts: Create a new blog post. --------
-// PUT /posts/:id: Update an existing blog post. -- Zeev said not to do this
-// DELETE /posts/:id: Delete a blog post.-------
-
 const express = require('express');
+const bp = require('body-parser');
+
 const app = express();
+app.use(bp.urlencoded({extended: true}));
+app.use(bp.json());
 
 const data = [
     {id:1, title: "What is happened?", content: "I really understand nothing about this new world"},
@@ -21,22 +17,40 @@ const data = [
 
 app.listen(3001);
 
+// GET /posts: Return a list of all blog posts.
 app.get('/posts', (request, response) => {
-    try{
-        response.json(data)
-    }
-    catch(err){
-        console.log(`I can't retun tour data because ${err.message}`)
-    }
+    if (!data || data.length === 0) response.status(404).send('Data not found');
+    response.json(data);
 });
 
+// GET /posts/:id: Return a specific blog post based on its id.
 app.get('/posts/:id', (request, response) => {
-    try{ 
-        const post = data.find(post => post.id == request.params.id)
-        if(!post) return response.status(404).send('User not found')
-        response.json(post)
-    }
-    catch(err){
-        console.log(`I can't retun tour data because ${err.message}`)
-    }
+    const post = data.find(post => post.id == request.params.id);
+    if(!post) return response.status(404).send('Post not found');
+    response.json(post);
+});
+
+
+// POST /posts: Create a new blog post. 
+app.post('/posts', (request, response) => {
+    const newPost = {...request.body, id: data.length+1};
+    data.push(newPost)
+    response.json(data);
+});
+
+// PUT /posts/:id: Update an existing blog post. 
+app.put('/posts/:id', (request, response) => {
+    let post = data.find(post => post.id == request.params.id);
+    if(!post) return response.status(404).send('Post not found');
+    const {title, content} = request.body;
+    post = {...post, title, content};
+    response.json(post);
+});
+
+// DELETE /posts/:id: Delete a blog post.
+app.delete('/posts/:id', (request, response) =>{
+    const postInd = data.findIndex(item => item.id == request.params.id);
+    if (postInd == -1) return response.status(404).send('Post not found');
+    data.splice(postInd, 1)
+    response.json(data);
 });
