33ba00a700fb6510b3bb6550ba717d9147ccc9f3
Sveta Livshits <nekto.nap@gmail.com>
2024-01-28 16:12:06 +0200

    week11 day1 Exercise XP done

--- Week11/Day1/exercise-xp/src/components.js/Todo.js
+++ Week11/Day1/exercise-xp/src/components.js/Todo.js
@@ -18,18 +18,24 @@ import {
         <>
         <input type="text" placeholder="Todo" ref={textRef}/>
         <button onClick={()=>{dispatch(addTodo(Date.now(), textRef.current.value)); textRef.current.value=''}}>+</button> 
-        {state.map((todo) => (
-          <div key={todo.id}>
-            <div>{todo.text}</div>
-            {todo.isDone===true? 'Done':'Active'}
-            <input type="checkbox" onChange={() => dispatch(toggleTodo(todo.id))}/>
-            <br/>
-            <button onClick={() => dispatch(deleteTodo(todo.id))}>Remove</button>
-            <br/>
-            <br/>
-            <br/>
-          </div>
-        ))}
+        {state.map((todo) => {
+            if (todo.text) {
+                return (
+                    <div key={todo.id}>
+                        <div>{todo.text}</div>
+                        {todo.isDone===true? 'Done':'Active'}
+                        <input type="checkbox" onChange={() => dispatch(toggleTodo(todo.id))}/>
+                        <br/>
+                        <button onClick={() => dispatch(deleteTodo(todo.id))}>Remove</button>
+                        <br/>
+                        <br/>
+                        <br/>
+                    </div>
+                    )
+                }
+            return null;
+            } 
+        )}
         </>
     )
 }
